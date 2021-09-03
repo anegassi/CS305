3957cd0f47b4f92f15d9c8089483a1f012d3101c
anegassi <89844342+anegassi@users.noreply.github.com>
2021-09-02 19:00:59 -0700

    Update exercise.js

--- assignment/lab4/exercise.js
+++ assignment/lab4/exercise.js
@@ -8,7 +8,7 @@ function sumMultiDimentionalArray(myArray){
             sum+= myArray[i][k];
         }
     }
-    return sum;
+    return;
 }
 
 console.log(sumMultiDimentionalArray([[1,2,3],[4,5],[6,4,7]]));
